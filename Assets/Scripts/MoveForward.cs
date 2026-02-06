f588b808541b03d4638d5715747ec7d3ea4cabce
pastsawutkhon <pastsawut.khon@bumail.net>
2026-02-06 16:26:18 +0700

    wip

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 40.0f;
+    
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
